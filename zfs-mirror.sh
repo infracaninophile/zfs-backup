b96ea591aaf784173ffed0e6217031ff1479297a
Matthew Seaman <m.seaman@infracaninophile.co.uk>
2016-03-15 15:21:15 +0000

    add some utility functions to extract snapshots / bookmarks related to mirroring.
    Whitespace fixes.

--- zfs-mirror.sh
+++ zfs-mirror.sh
@@ -62,7 +62,7 @@ runv() {
 }
 
 # Identify which ZFS is mounted containing the path of interest (not
-# limited to ZFS mountpoints).  
+# limited to ZFS mountpoints).
 path_to_zfs() {
     local var_return="$1"
     local path="$2"
@@ -115,17 +115,61 @@ get_tag_from() {
 # should always return a snapshot, unless things have gone pretty awry
 # for quite some time.  However, we're OK if it returns a bookmark.
 get_latest_mirror() {
-    local var_return= "$1"
+    local var_return="$1"
     local zfs="$2"
     local latest
-    
+
     latest=$( zfs list -H -d 1 -t snapshot,bookmark -o name -S creation | \
 		    grep -E "[@#]$snapmatch" | head -1 )
 
     setvar "$var_return" "$latest"
 }
 
-# @@@@@
+# Get all of the snapnames used for mirroring of a particular ZFS.
+# Choose from snapshots, bookmarks or both and order olded or newest
+# first.
+get_all_mirrors() {
+    local var_return="$1"
+    local zfs="$2"
+    local type="$3"
+    local order="$4"
+
+    local sort_order
+    local zobj
+
+    case "$order" in
+	reversed|descending)
+	    sort_order='-s creation' # Oldest first
+	    ;;
+	normal|ascending)
+	    sort_order='-S creation' # Newest first
+	    ;;
+	*)
+	    echo >&2 "$ME: Sort order $order not understood:" \
+		     "try one of normal, reversed. ascending, descending"
+	    exit 1
+	    ;;
+    esac
+
+    case "$type" in
+	all)
+	    type='snapshot,bookmark'
+	    ;;
+	bookmark|snapshot)
+	    ;;
+	*)
+	    echo >&2 "$ME: $type not understood:" \
+		     "try one of 'all', 'bookmark' or 'snapshot'"
+	    exit 1
+	    ;;
+    esac
+
+    zobj=$( zfs list -H -t $type $sort_order -o name -d 1 $zfs | \
+		  grep -E "[@#]$snap_match" )
+
+    setvar "$var_return" "$zobj"
+}
+
 
 #
 # Main program starts here
